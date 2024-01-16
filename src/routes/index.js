fc0565190eff0dc02c8d4aca782bf1e29f72f190
Ubuntu <ubuntu@ip-172-31-32-31.sa-east-1.compute.internal>
2024-01-16 17:58:02 +0000

    The AuthController class was created with the login method.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,14 @@ const { Router } = require("express");
 const router = Router();
 
 const DogController = require("../controllers/dogController");
+const AuthController = require("../controllers/authController");
 const dog = new DogController();
+const auth = new AuthController();
 
+// Authentication
+router.get("/auth", (req, res) => auth.login(req, res));
 
 router.get("/dogs/breedName", (req, res) => dog.getBreedByName(req, res));
-/*router.get("/dogs", (req, res) => dog.getAllBreeds(req, res));
+router.get("/dogs", (req, res) => dog.getAllBreeds(req, res));
 router.get("/dogs/:idBreed", (req, res) => dog.getBreedById(req, res));
-*/
 module.exports = router;
